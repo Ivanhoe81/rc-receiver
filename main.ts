1db255806d3afae32517cd0559538b813347e6f8
Ivanhoe81 <42270386+Ivanhoe81@users.noreply.github.com>
2021-12-31 14:57:32 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         wuKong.setAllMotor(value, value)
     }
+    if (name == "x") {
+        wuKong.setServoAngle(wuKong.ServoTypeList._270, wuKong.ServoList.S0, value)
+    }
     if (name == "duda") {
         if (value == 1) {
             music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
