26e30a0c1b9c1fd5722d349b81bb765aef6a2134
Ivanhoe81 <42270386+Ivanhoe81@users.noreply.github.com>
2021-12-31 13:14:15 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -2,5 +2,10 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         wuKong.setAllMotor(value, value)
     }
+    if (name == "duda") {
+        if (value == 1) {
+            music.startMelody(music.builtInMelody(Melodies.BaDing), MelodyOptions.Once)
+        }
+    }
 })
 radio.setGroup(1)
