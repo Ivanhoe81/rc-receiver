9f6af4bb0648b8213f601813b072b7d0237d84ed
Ivanhoe81 <42270386+Ivanhoe81@users.noreply.github.com>
2022-01-01 16:35:45 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -13,12 +13,12 @@ radio.onReceivedValue(function (name, value) {
         }
     }
 })
-let s2c = 0
-let s1c = 0
 let steer = 0
 let m2 = 0
 let m1 = 0
 radio.setGroup(1)
+let s1c = 1
+let s2c = 1
 basic.forever(function () {
     if (m1 > 1) {
         m1 = m1 * s1c
@@ -35,18 +35,20 @@ basic.forever(function () {
     if (steer >= 205) {
         s2c = Math.map(steer, 205, 245, 1, 0.5)
     }
+})
+basic.forever(function () {
     if (m1 > 5) {
-        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 30) {
-            music.playMelody("- C5 - C5 - C5 - C5 ", 120)
+        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 40) {
+            music.playTone(988, music.beat(BeatFraction.Half))
+            music.rest(music.beat(BeatFraction.Whole))
         }
         if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 20) {
-            music.playMelody("- C5 - C5 - C5 - C5 ", 180)
+            music.playTone(988, music.beat(BeatFraction.Half))
+            music.rest(music.beat(BeatFraction.Quarter))
         }
         if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 10) {
-            music.playMelody("- C5 - C5 - C5 - C5 ", 240)
-        }
-        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 10) {
-            music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 120)
+            music.playTone(988, music.beat(BeatFraction.Half))
+            music.rest(music.beat(BeatFraction.Sixteenth))
         }
     }
 })
