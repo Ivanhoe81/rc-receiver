234ef7c53f3d27445703f49139510a8ff30f0ccd
Ivanhoe81 <42270386+Ivanhoe81@users.noreply.github.com>
2022-01-01 14:52:37 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 radio.onReceivedValue(function (name, value) {
     if (name == "y") {
-        wuKong.setAllMotor(value, value)
+        m1 = value
+        m2 = value
     }
     if (name == "x") {
         wuKong.setServoAngle(wuKong.ServoTypeList._270, wuKong.ServoList.S0, value)
+        steer = value
     }
     if (name == "duda") {
         if (value == 1) {
@@ -11,4 +13,40 @@ radio.onReceivedValue(function (name, value) {
         }
     }
 })
+let s2c = 0
+let s1c = 0
+let steer = 0
+let m2 = 0
+let m1 = 0
 radio.setGroup(1)
+basic.forever(function () {
+    if (m1 > 1) {
+        m1 = m1 * s1c
+        m2 = m2 * s2c
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, m1 * 0.3)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, m2 * 0.3)
+    } else {
+        wuKong.setMotorSpeed(wuKong.MotorList.M1, m1 * s1c)
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, m2 * s2c)
+    }
+    if (steer <= 205) {
+        s1c = Math.map(steer, 205, 165, 1, 0.5)
+    }
+    if (steer >= 205) {
+        s2c = Math.map(steer, 205, 245, 1, 0.5)
+    }
+    if (m1 > 5) {
+        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 30) {
+            music.playMelody("- C5 - C5 - C5 - C5 ", 120)
+        }
+        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 20) {
+            music.playMelody("- C5 - C5 - C5 - C5 ", 180)
+        }
+        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 10) {
+            music.playMelody("- C5 - C5 - C5 - C5 ", 240)
+        }
+        if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 10) {
+            music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 120)
+        }
+    }
+})
